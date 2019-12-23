ac6f488126a6a37ccefe0b1842df02b4d2387ba1
Sidhant Puntambekar <sidhantnp@yahoo.com>
2019-12-23 09:18:02 -0700

    Initialized sports stack and created print method

--- StacksPractice.cpp
+++ StacksPractice.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <stack>
 using namespace std;
+
+void printStack(stack <string> s) 
+{ 
+    while (!s.empty()) 
+    { 
+        cout << '\t' << s.top(); 
+        s.pop(); 
+    } 
+    cout << '\n'; 
+} 
 
 int main()
 {
-    
+    stack <string> sports;
+    sports.push("Basketball");
+    sports.push("Football");
+    sports.push("Mixed Martial Arts");
+    sports.push("Boxing");
+    sports.push("Futbol");
+
+    cout << "The stack is: " << endl;
+    printStack(sports);
 }
